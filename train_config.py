347e6bd8259f17f5c1b6fe575e04c5dd4c549add
UygarUsta <34573541+UygarUsta@users.noreply.github.com>
2024-11-17 18:41:30 +0300

    Update train_config.py

--- train_config.py
+++ train_config.py
@@ -6,7 +6,7 @@ from configs.mscoco.mbv2_config import config as mb2_config
 from configs.mscoco.shufflenet_5x5_config import config as shufflenet_config
 from configs.mscoco.resnet18_config import config as resnet_config
 ##### the config for different task
-config=shufflenet_config
+config=mb2_config
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 config.TRAIN.num_gpu = 1
