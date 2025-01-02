6b3e14ae8e512d34f6203f9dbfe95163bcbf2ac6
UygarUsta <34573541+UygarUsta@users.noreply.github.com>
2025-01-02 09:14:35 +0300

    Add files via upload

--- inference.py
+++ inference.py
@@ -15,11 +15,13 @@ for i in f:
 
 print(classes)
 
+input_height = 512
+input_width = 512
 
-folder = r"C:\Users\PC\Pictures\_" #r"G:\COCO\val2017" #"E:/derpetv5_xml" #"/home/rivian/Desktop/Datasets/derpet_v4_label_tf" #"/home/rivian/Desktop/Datasets/coco_mini_train"
+folder = r"G:\COCO\val2017" #r"E:\derpetv5_xml\val_images" #r"G:\COCO\val2017" #"E:/derpetv5_xml" #"/home/rivian/Desktop/Datasets/derpet_v4_label_tf" #"/home/rivian/Desktop/Datasets/coco_mini_train"
 #folder = os.path.join(folder,"val_images") #place to val
 
-model_path = "last_epoch_weights.pth" #"pretrained-hardnet.pth"
+model_path = "best_epoch_weights_ciou_fe_mbv2_shufflenet.pth" #"pretrained-hardnet.pth"
 device = "cuda"
 model_type = "shufflenet"
 
@@ -103,8 +105,6 @@ if half:
     model.half()
 
 if trace:
-    input_height = 320
-    input_width = 320
     dummy_input = torch.randn(1, 3, input_height, input_width).to(device)
     print("Start Tracing")
     model = torch.jit.trace(model, dummy_input)
@@ -112,20 +112,16 @@ if trace:
 
 if openvino_exp:
     import openvino as ov
-    input_height = 512
-    input_width = 512
     dummy_input = torch.randn(1, 3, input_height, input_width).to(device)
     model =  ov.compile_model(ov.convert_model(model, example_input=dummy_input))
 
 
 if export_onnx:
-    input_width = 512
-    input_height = 512
     torch_input = torch.randn(1, 3, input_width, input_height)
     onnx_program = torch.onnx.dynamo_export(model, torch_input)
     #onnx_program.save("mbv2_shufflenet_widerface.onnx")
 
-save_xml = True
+save_xml = False
 if save_xml :
     if not os.path.isdir(os.path.join(folder,"annos")):
         os.mkdir(os.path.join(folder,"annos"))
@@ -133,16 +129,17 @@ if save_xml :
     convert = Converter(os.path.join(folder,"annos"))
     
 
-#video_path = r"E:\ESHOT 2024\Geshot Gediz 2 Garaji Gorselleri\Videolar\Otokar Ters Depo.avi"
-video_path = r"G:\0010.mp4" #0 #r"G:\8_2023-07-31-11.37.01_novis_output.avi"
+video_path = r"E:\ESHOT 2024\Geshot Gediz 2 Garaji Gorselleri\Videolar\Otokar Ters Depo.avi"
+#video_path = 0 #r"G:\youtube-dl\Tutorial Shorts - Filling Up At Pump-mIc1TD6c7Ig.mp4" #0 #r"G:\8_2023-07-31-11.37.01_novis_output.avi"
 if video:
     cap = cv2.VideoCapture(video_path)
     while 1:
         ret,img = cap.read()
         #img = cv2.resize(img,(1280,720))
+        #img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE) #for some videos
         img = img[...,::-1]
-        image,annos = infer_image(model,img,classes,conf,half,input_shape=(320,320),cpu=cpu,openvino_exp=openvino_exp)
-        image = cv2.resize(image,(1280,720))
+        image,annos = infer_image(model,img,classes,conf,half,input_shape=(input_height,input_width),cpu=cpu,openvino_exp=openvino_exp)
+        #image = cv2.resize(image,(1280,720))
         #print(annos)
         cv2.imshow("img",image[...,::-1])
         ch = cv2.waitKey(1)
@@ -155,7 +152,7 @@ else:
         print(i)
         if save_xml:
             annotations = []
-        image,annos = infer_image(model,i,classes,conf,half,input_shape=(512,512),cpu=cpu,openvino_exp=openvino_exp)
+        image,annos = infer_image(model,i,classes,conf,half,input_shape=(input_height,input_width),cpu=cpu,openvino_exp=openvino_exp)
         if save_xml:
             for b in annos:
                 xmin = b[0]
@@ -164,7 +161,7 @@ else:
                 ymax = b[3]
                 class_ = b[4]
                 annotations.append([xmin,ymin,xmax,ymax,class_])
-        image = cv2.resize(image,(1280,720))
+        #image = cv2.resize(image,(1280,720))
         cv2.imshow("img",image[...,::-1])
         ch = cv2.waitKey(0)
         if ch == ord("q"): break
