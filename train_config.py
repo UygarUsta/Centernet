1a117433857ef8c2f149dd78d605f6b498a8c2c3
UygarUsta <34573541+UygarUsta@users.noreply.github.com>
2025-01-05 00:34:09 +0300

    Update train_config.py

--- train_config.py
+++ train_config.py
@@ -3,6 +3,7 @@
 import os
 
 from configs.mscoco.mbv2_config import config as mb2_config
+from configs.mscoco.mbv4_config import config as mb4_config
 from configs.mscoco.shufflenet_5x5_config import config as shufflenet_config
 from configs.mscoco.resnet18_config import config as resnet_config
 ##### the config for different task
